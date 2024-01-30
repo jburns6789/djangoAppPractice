1f83a2365c204bd5bd41022040895057ac58bde4
[joel] <[jbur645@wgu.edu]>
2024-01-30 11:44:47 -0500

    production configurations

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4w&ixzmbe#y11*@zrcde9bo1=nmiavn+ba+x_wuenwat&f05zb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
